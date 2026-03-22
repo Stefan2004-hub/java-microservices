ea7bbb0690d65bd6b75831db1c32afef8a84c0c3
Stefan Dumitriu <Stefan.Dumitriu@hartehanks.com>
2026-03-23 01:10:12 +0200

    add eureka server

--- order-service/src/main/java/com/example/order_service/client/ProductClient.java
+++ order-service/src/main/java/com/example/order_service/client/ProductClient.java
@@ -5,8 +5,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "product-client", url = "http://localhost:8081/products")
+// @FeignClient(name = "product-client", url = "http://localhost:8081/products")
+@FeignClient(name = "product-service")
 public interface ProductClient {
-  @GetMapping("/{id}")
+  @GetMapping("/products/{id}")
   ProductResponse getProduct(@PathVariable Long id);
 }
